61dcd5d3ccff38ef6586ea7f8b189258eb36694a
akmxx46 <procding@gmail.com>
2024-12-01 09:59:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ const usernameInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
 const errorMessage = document.getElementById("errorMessage");
 
+
 loginForm.addEventListener("submit", function (e) {
-  e.preventDefault(); // Mencegah form refresh
+  e.preventDefault(); 
   const enteredUsername = usernameInput.value;
   const enteredPassword = passwordInput.value;
 
   if (enteredUsername === correctUsername && enteredPassword === correctPassword) {
+
     window.location.href = "https://www.tiktok.com/@malware79?_t=8rqF02xuQtN&_r=1";
   } else {
-    // Tampilkan pesan error jika username atau password salah
+
     errorMessage.style.display = "block";
     errorMessage.textContent = "Username atau password salah!";
   }
